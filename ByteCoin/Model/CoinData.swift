825979d3ded54a46eb0099d2c93623a5b40cbc1a
Ko Ko Aung <kaung@oswego.edu>
2024-06-14 20:04:05 -0700

    first commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ob yda on 6/1/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Ob yda on 6/1/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel {
+    let coinName: String
+    let exchangeRate: Double
+}
